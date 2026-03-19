8c25b9e5a8bac7d50ac03a70f18aa55438b6a8ad
Shahin Raj <63536425+shahinraj42@users.noreply.github.com>
2026-03-20 01:33:45 +0600

    2nd backend upload
    2nd backend.

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Temporary database
+let menu = [
+  { name: "Burger", price: 200 },
+  { name: "Coffee", price: 100 }
+];
+
+// Get menu
+app.get("/menu", (req, res) => {
+  res.json(menu);
+});
+
+// Add new item
+app.post("/menu", (req, res) => {
+  menu.push(req.body);
+  res.json({ message: "Item added" });
+});
+
+
+
+let orders = [];
+
+// Receive order
+app.post("/order", (req, res) => {
+  const newOrder = {
+    id: Date.now(),
+    items: req.body.items,
+    table: req.body.table,
+    status: "Pending"
+  };
+
+  orders.push(newOrder);
+
+  console.log("New Order Received:", newOrder);
+
+  res.json({ message: "Order received" });
+});
+
+// Send all orders (for kitchen)
+app.get("/order", (req, res) => {
+  res.json(orders);
+});
+
+
+app.put("/order/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const order = orders.find(o => o.id === id);
+
+  if (order) {
+    order.status = req.body.status;
+    res.json({ message: "Updated" });
+  } else {
+    res.status(404).json({ message: "Order not found" });
+  }
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
